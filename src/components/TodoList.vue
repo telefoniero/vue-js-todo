06324ecac39a9c4888330d230e8190109397b457
Alex Topunov <telefoniero@github.com>
2022-09-08 15:15:59 +0300

    sorting

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,15 @@
 <template>
   <ul class="todo-list">
-    <li class="todo-list__item todo-list__item_title">
+    <li class="todo-list__item todo-list__item_header">
       <h3 class="todo-list__title" v-if="title" v-text="title" />
+      <button type="button" class="todo-list__sort-btn" @click="sort">
+        sort {{ sortDisplay }}
+      </button>
     </li>
     <transition-group name="fade">
       <li
         class="todo-list__item todo-list__item_in-progress"
-        v-for="todo of todos"
+        v-for="todo of sortedTodos"
         :key="todo.id"
         v-show="todos.length"
       >
@@ -30,11 +33,28 @@ export default {
   },
   data: () => {
     return {
-      sortOrder: "asc"
+      sortOrder: 0
     };
   },
+  methods: {
+    sort() {
+      if (!this.sortOrder) this.sortOrder = 1;
+      else this.sortOrder = -this.sortOrder;
+    }
+  },
   computed: {
-    sortedTodos() {}
+    sortedTodos() {
+      const todosCopy = [...this.$props.todos];
+      todosCopy.sort((a, b) => {
+        if (a.title < b.title) return -1 * this.sortOrder;
+        else if (a.title > b.title) return 1 * this.sortOrder;
+      });
+      return todosCopy;
+    },
+    sortDisplay() {
+      if (this.sortOrder === 1) return "(asc)";
+      else if (this.sortOrder === -1) return "(desc)";
+    }
   },
   components: { TodoItem }
 };
